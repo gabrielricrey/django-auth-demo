f0d8ca14637280da0329ae98f1052608438df563
Gabriel Reynoso <garey91@gmail.com>
2025-08-11 15:39:45 +0200

    Update ALLOWED_HOSTS in settings.py for deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l8l=9av4hm)gph%9k6dt9m03xi9n1%xg+j+dwh0k9l)%1(wyxm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-auth-demo.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
